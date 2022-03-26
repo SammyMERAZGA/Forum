f786d4d641f60f1b5dc791fbe06b5ec176657ab5
SammyMERAZGA <sammy.merazga@gmail.com>
2022-03-26 19:30:20 +0100

    User methods done

--- models/User.php
+++ models/User.php
@@ -32,9 +32,84 @@ function getUserInfo($pseudo)
 }
 
 // Get all users (for back-office)
+function getAllUsers()
+{
+  $connexion = SGBDConnect();
+
+  $requete = 'SELECT user_id, lastname, firstname, pseudo, email, admin'
+          .  ' FROM user';
+
+  $resultat = $connexion->query($requete);
+  $resultat->setFetchMode(PDO::FETCH_ASSOC);
+  $lignes = $resultat->fetchAll(PDO::FETCH_ASSOC);
+  return $lignes;
+}
 
 // Update user
+function setUser($lastname, $firstname, $pseudo, $email, $password, $user_id)
+{
+  try
+  {
+    $connexion = SGBDConnect();
+
+    $requete = 'UPDATE user'
+            . ' SET lastname = "' . $lastname
+            . '", firstname = "' . $firstname
+            . '", pseudo = "' . $pseudo
+            . '", email = "' . $email
+            . '", password = "' . $password
+            . '" WHERE user_id = ' . $user_id;
+    $resultat = $connexion->query($requete);
+  }
+  catch (PDOException $e)
+  {
+    echo 'Erreur !: ' . $e->getMessage() . '<br />';
+    exit();
+  }
+return $resultat;
+}
+
+// Update role of user for admin
+function setUserRole($lastname, $firstname, $pseudo, $email, $password, $admin, $user_id)
+{
+  try
+  {
+    $connexion = SGBDConnect();
+
+    $requete = 'UPDATE user'
+            . ' SET lastname = "' . $lastname
+            . '", firstname = "' . $firstname
+            . '", pseudo = "' . $pseudo
+            . '", email = "' . $email
+            . '", password = "' . $password
+            . '", admin = ' . $admin
+            . '" WHERE user_id = ' . $user_id;
+    $resultat = $connexion->query($requete);
+  }
+  catch (PDOException $e)
+  {
+    echo 'Erreur !: ' . $e->getMessage() . '<br />';
+    exit();
+  }
+return $resultat;
+}
 
 // Delete user (only admin)
+function deleteUser($userId)
+{
+  try
+  {
+    $connexion = SGBDConnect();
+
+    $requete = 'DELETE FROM user WHERE user_id = ' . $userId . '';
+    $resultat = $connexion->query($requete);
+  }
+  catch (PDOException $e)
+  {
+    echo 'Erreur !: ' . $e->getMessage() . '<br />';
+    exit();
+  }
+  return $resultat;
+}
 
 ?>
