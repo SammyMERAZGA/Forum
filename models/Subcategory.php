cffb5736bc8f9f4656ea2a1611f5fd6164c69c3e
SammyMERAZGA <sammy.merazga@gmail.com>
2022-03-26 19:26:06 +0100

    Subcategory methods done

--- models/Subcategory.php
+++ models/Subcategory.php
@@ -3,13 +3,74 @@
 require_once ("./models/database/DataSource.php");
 
 // GET ALL
+function getAllSubcategory()
+{
+  $connexion = SGBDConnect();
+
+  $requete = 'SELECT subcategory_id, title, description, category_id, user_id'
+          .  ' FROM subcategory';
+
+  $resultat = $connexion->query($requete);
+  $resultat->setFetchMode(PDO::FETCH_ASSOC);
+  $lignes = $resultat->fetchAll(PDO::FETCH_ASSOC);
+  return $lignes;
+}
 
 // GET
+function getSubategory($subcategoryId)
+{
+  $connexion = SGBDConnect();
 
+  $requete = 'SELECT subcategory_id, title, description, category_id, user_id'
+          . ' FROM subcategory'
+          . ' WHERE subcategory_id = "' .$subcategoryId. '"';
+
+  $resultat = $connexion->query($requete);
+  $resultat->setFetchMode(PDO::FETCH_ASSOC);
+  $ligne = $resultat->fetch(PDO::FETCH_ASSOC);
+  return $ligne;
+}
+
+// add admin verification
 // SET (UPDATE) only admin
+function setSubcategory($title, $description, $category_id, $subcategory_id)
+{
+  try
+  {
+    $connexion = SGBDConnect();
 
-// UPDATE only admin
+    $requete = 'UPDATE subcategory'
+            . ' SET title = "' . $title
+            . '", description = "' . $description
+            . '", category_id = ' . $category_id
+            . ' WHERE subcategory_id = ' . $subcategory_id;
+    $resultat = $connexion->query($requete);
+  }
+  catch (PDOException $e)
+  {
+    echo 'Erreur !: ' . $e->getMessage() . '<br />';
+    exit();
+  }
+return $resultat;
+}
 
+// add admin verification
 // DELETE only admin
+function deleteCategory($subategoryId)
+{
+  try
+  {
+    $connexion = SGBDConnect();
+
+    $requete = 'DELETE FROM subcategory WHERE subcategory_id = ' . $subategoryId . '';
+    $resultat = $connexion->query($requete);
+  }
+  catch (PDOException $e)
+  {
+    echo 'Erreur !: ' . $e->getMessage() . '<br />';
+    exit();
+  }
+  return $resultat;
+}
 
 ?>
